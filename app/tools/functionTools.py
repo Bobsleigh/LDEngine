81e8dc45345aa657c5cf1c4734ef3f7052833d41
Benoit-Pouliot <pouliot.benoit@gmail.com>
2016-12-04 19:42:01 -0500

    sys.exit

--- app/tools/functionTools.py
+++ app/tools/functionTools.py
@@ -1,4 +1,4 @@
-from sys import exit
+import sys
 
 # seek the attribute name in object, if none, pass
 def seekAtt(objectLocal, nameAttribute):
@@ -7,6 +7,6 @@ def seekAtt(objectLocal, nameAttribute):
     except AttributeError:
         return None
 
-# quit game ( NEVER USE quit() )
+# quit game ( NEVER USE quit() or exit() ), we use sys.exit()
 def quitGame():
-    exit()
+    sys.exit()
