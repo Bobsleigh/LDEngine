2b36a54c06f2bb8caa69bfba25722128fe0108f0
Benoit-Pouliot <pouliot.benoit@gmail.com>
2017-07-16 15:34:38 -0400

    Add a spawn point (InZone) for the TileColisions/Test

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ if __name__ == '__main__':
 
     # Uncomment the test you want to run. This main file is needed to keep the relative path intact.
 
-    exec(open('FeatureTests\TileCollisions\Test.py').read())
+    #exec(open('FeatureTests\TileCollisions\Test.py').read())
     #exec(open('FeatureTests\DialogBox\Test.py').read())
+
+    if TAG_BP:
+        exec(open('FeatureTests\TileCollisions\Test.py').read())
+        #exec(open('FeatureTests\DialogBox\Test.py').read())
